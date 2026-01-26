5d33b1ee215dff2e0fd825ead07cbba72d5741f9
Arjun <singh.arjun@rutgers.edu>
2026-01-25 20:26:05 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open("patients.json", 'r') as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Arjun"}
+    return {"message": "Patient Management System"}
 
 @app.get("/about")
-def intro():
-    return {"message": "This is practice notebook to learn about the fastapi."}
+def about():
+    return {"message": "A fullly functional patient management system built with FastAPI."}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str):
+    data = load_data()
+    patient_ = data.get(patient_id)
+    if patient_ is not None:
+        return patient_
+    else: 
+        raise HTTPException(status_code = 404, detail = "Patient data is not available. Please add the right name.")
+    
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description = "Sort the query based on height, bmi, and, weight."), order_by: str = Query("asc", description = "You want to sort in asc or des order")):
+    data = load_data()
+    validate_sort_by = ["height", "weight", "bmi"]
+    if sort_by not in validate_sort_by:
+        raise HTTPException(status_code = 400, detail = f"Please choose from {validate_sort_by}")
+    if order_by not in ['asc', 'dsc']:
+        raise HTTPException(status_code = 400, detail = f"Please choose from 'asc' or 'dsc'")
+    
+    sorted_data = sorted(data.values(), key = lambda x: x[sort_by], reverse = (order_by == "dsc"))
+    return sorted_data
